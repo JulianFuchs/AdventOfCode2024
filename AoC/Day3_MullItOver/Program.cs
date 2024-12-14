a003727d0007e9db5dde5e1acd8f0540a58c57dc
j.fuchs <j.fuchs@criteo.com>
2024-12-14 19:46:37 +0100

    🎄 First ⭐ for Day 3 with Regex

--- AoC/Day3_MullItOver/Program.cs
+++ AoC/Day3_MullItOver/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Day1_HistorianHysteria.CommonHelpers;
+using Day3_MullItOver;
+
+Console.WriteLine("Solving for first star *");
+
+var input = FileInputReader.ReadInputFile();
+
+var solution1 = SolverFirstStar.Solve(input);
+
+Console.WriteLine($"Solution for first star *: {solution1}");
+
---
+++ AoC/Day3_MullItOver/SolverFirstStar.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day3_MullItOver;
+
+internal static class SolverFirstStar
+{
+    internal static double Solve(string[] inputLines)
+    {
+        var result = 0d;
+        
+        const string pattern = @"mul\((\d*),(\d*)\)";
+
+        Regex regex = new Regex(pattern);
+        
+        foreach (var inputLine in inputLines)
+        {
+            var matches = regex.Matches(inputLine);
+
+            foreach (Match match in matches)
+            {;
+                var value1 = match.Groups[1].Value;
+                var value2 = match.Groups[2].Value;
+
+                result += int.Parse(value1) * int.Parse(value2);
+            }
+        }
+        
+        
+        return result;
+    }
+}
