9849561ae1a98dbf883ab1a43f76e051e2ad9602
j.fuchs <j.fuchs@criteo.com>
2024-12-14 19:09:16 +0100

    🚚 Changing structure

---
+++ AoC/HelperLib/FileInputReader.cs
@@ -0,0 +1,15 @@
+﻿namespace Day1_HistorianHysteria.CommonHelpers
+{
+    public static class FileInputReader
+    {
+        public static string[] ReadInputFile()
+        {
+            var filePath = "input.txt";
+
+            string[] lines = File.ReadAllLines(filePath);
+            
+            return lines;
+        }
+
+    }
+}
---
+++ AoC/HelperLib/IInputConverter.cs
@@ -0,0 +1,7 @@
+﻿namespace Day1_HistorianHysteria.CommonHelpers
+{
+    public interface IInputConverter<T>
+    {
+        public T ConvertInput(string[] input);
+    }
+}
